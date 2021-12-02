5f8af2443f53633ab757af83c1eadc7c63340f5c
techwithlik <hfylt4@nottingham.edu.my>
2021-12-02 17:02:25 +0800

    Increased radius of the ball

--- src/Main/Ball/RubberBall.java
+++ src/Main/Ball/RubberBall.java
@@ -23,7 +23,7 @@ import java.awt.geom.Point2D;
 
 public class RubberBall extends Ball {
 
-    private static final int DEF_RADIUS = 10;
+    private static final int DEF_RADIUS = 12;
     private static final Color DEF_INNER_COLOR = new Color(255, 219, 88);
     private static final Color DEF_BORDER_COLOR = DEF_INNER_COLOR.darker().darker();
 
